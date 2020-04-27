369a984b823da1a090e0bad2d42a40c2a5dc2f03
linyu203 <linyu203@gmail.com>
2020-04-27 00:01:38 -0400

    add rules

--- cpp/rules/rule.cpp
+++ cpp/rules/rule.cpp
@@ -1,14 +1,23 @@
+
 #include "invalidexecution.h"
+#include "mismatchedexecution.h"
+#include "oversizedexecution.h"
+#include "orderexpired.h"
+#include "sellforprofit.h"
 #include "abnormalprice.h"
-
-
+#include "domination.h"
 
 std::vector<std::shared_ptr<Rule>> initRules(
     std::vector<std::shared_ptr<Action>>& act, 
     const Market& mkt) {
     std::vector<std::shared_ptr<Rule>> rules;
     rules.push_back(std::make_shared<InvalidExecution>(act,mkt));
+    rules.push_back(std::make_shared<MismatchedExecution>(act,mkt));
+    rules.push_back(std::make_shared<OversizedExecution>(act,mkt));
+    rules.push_back(std::make_shared<OrderExpired>(act,mkt));
+    rules.push_back(std::make_shared<SellForProfit>(act,mkt));
     rules.push_back(std::make_shared<AbnormalPrice>(act,mkt));
+    rules.push_back(std::make_shared<Domination>(act,mkt));
     return rules;
 }
 
