77aabc8b05ca69085c55882865fda6ec7fed0deb
linyu203 <linyu203@gmail.com>
2020-04-27 19:53:10 -0400

    add domination and sellforprofit

--- python/rules/dmn.py
+++ python/rules/dmn.py
@@ -1,11 +1,59 @@
 from event import Event
 
+
+class HisVolum:
+    def __init__(self):
+        self.d_sum = 0
+        self.d_curVol = 0
+        self.d_hisVol = list()
+
+    def increment(self, vol):
+        self.d_curVol += vol
+
+    def update(self):
+        if self.d_curVol == 0:
+            return False
+        self.d_sum += self.d_curVol
+        self.d_hisVol.append(self.d_curVol)
+        self.d_curVol = 0
+        if len(self.d_hisVol) == 5:
+            self.d_sum -= self.d_hisVol[0]
+            self.d_hisVol.remove(0)
+        return true;
+
+    def getSumAndCnt(self):
+        return self.d_sum, len(self.d_hisVol)
+
+        
+        
 class DMN:
     def __init__(self):
-        self.d_orderDict = dict()
+        self.d_curVolDict = dict()
+        self.d_curDate = -1
+        self.d_hisVolDict = dict()
 
+    def updateHis(self, curDate):
+        if curDate == self.d_curDate:
+            return
+        self.d_curDate = curDate
+        self.d_curVolDict.clear()
+        for key,val in self.d_hisVolDict.items():
+            if val.update():
+                self.d_hisVolDict[key] = val
+        
     def check(self, evn):
-        if evn.eventType == "EXECUTE":
-            pass
+        if evn.eventType != "EXECUTE":
+            return None
+
+        curDate = evn.dateTime.date()
+        updateHis(curDate)
+        key = evn.broker + "|" + evn.parsekey
+        tVol = self.d_curVolDict[key] += evn.volume
+        self.d_hisVolDict[evn.parsekey].increment(evn.volume)
+        sum, cnt = self.d_hisVolDict[evn.parsekey].getSumAndCnt()
+        if cnt>0 and cnt * tVol > 1.5 * sum:
+            ss = "D M N: {} has executed over 50% of the average volume for '{}', the total volume {}, the average is {:.2f}"
+            ss = ss.format(evn.broker, evn.parsekey, tVol, sum/cnt)
+            return ss
             
         return None
--- python/rules/sfp.py
+++ python/rules/sfp.py
@@ -2,10 +2,39 @@ from event import Event
 
 class SFP:
     def __init__(self):
-        self.d_orderDict = dict()
+        self.d_buyDict = dict()
 
     def check(self, evn):
-        if evn.eventType == "EXECUTE":
-            pass
-            
-        return None
+        if evn.eventType != "EXECUTE":
+            return None
+        key = evn.client + "|" + evn.parsekey
+        ss = ''
+        if evn.isBuy:
+            if key not in self.d_buyDict:
+                self.d_buyDict[key] = list()
+            self.d_buyDict[key].append([evn.price, evn.dateTime.date(), evn.volume])
+        else:
+            if key not in self.d_buyDict:
+                return None
+            ll = self.d_buyDict[key]
+            curVol = evn.volume
+            curDate = evn.dateTime.date()
+            curPrice = evn.price
+            while curVol > 0 && ll:
+                l0 = ll[0]
+                if ss == '' and l0[0] < curPrice and curDate - l0[1] < 30:
+                    "SFP: order CLIENT1 Sold 'SECURITY' within 30 days Bought on 01MAR2020 at $100.10 sold on 20MAR2020 at $100.20"
+                    ss = "SFP: order {} Sold '{}' within 30 days Bought on {} at ${:.2f} sold on {} at ${:.2f}"
+                    ss = ss.format(evn.client, evn.parsekey, l0[1].strftime("%d%b%Y").upper(), l0[0],
+                                   curDate.strftime("%d%b%Y").upper(), curPrice)
+                if curVol >= l0[2]:
+                    curVol -= l0[2]
+                    ll.remove(0)
+                else:
+                    l0[2] -= curVol
+                    ll[0] = l0
+                    break
+                    
+           self.d_buyDict[key] = ll
+           
+        return ss
